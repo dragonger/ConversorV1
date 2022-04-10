ca330abef5689da2fc3c33e680cc100fd5ec6682
dragonger <miguelbelizario144@gmail.com>
2022-04-09 22:43:39 -0300

    V1

---
+++ src/conversor/Conversor/Conversor.java
@@ -0,0 +1,17 @@
+package conversor.Conversor;
+
+import java.io.ObjectInputStream;
+import java.util.Scanner;
+
+public class Conversor {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Digite o valor do salario: ");
+	int salario = Integer.parseInt((teclado.nextLine()));
+        System.out.print("Digite o valor do dolar: ");
+    float dolar = Float.parseFloat(teclado.nextLine());
+    float conversão = salario* dolar;
+        System.out.printf("Salario em real é de %.2f \n ",conversão);
+    }
+}
---
+++ src/conversor/Conversor/ConversorTela.java
@@ -0,0 +1,8 @@
+package conversor.Conversor;
+
+import javax.swing.*;
+
+public class ConversorTela {
+    private JTextField txtsalario;
+    private JTextField textField1;
+}
