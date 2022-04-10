f056fff02f347344c133bb70708ca8635e76fa68
dragonger <miguelbelizario144@gmail.com>
2022-04-10 00:20:13 -0300

    Retirada do sout

--- src/conversor/Conversor/Conversor.java
+++ src/conversor/Conversor/Conversor.java
@@ -13,6 +13,5 @@ public class Conversor {
     float dolar = Float.parseFloat(teclado.nextLine());
     float conversão = salario* dolar;
         System.out.printf("Salario em real é de %.2f \n ",conversão);
-        System.out.println("Tudo bem gift hub?");
     }
 }
